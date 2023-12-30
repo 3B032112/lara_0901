d3a6020bfa234261b3078d33245e4cb54af912ad
3B032112 <rjoeyic@gmail.com>
2023-12-31 01:01:37 +0800

    Update admin Controller & its index to Read data

--- app/Http/Controllers/AdminHomeController.php
+++ app/Http/Controllers/AdminHomeController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class AdminHomeController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard.index');
+        $posts = Post::orderBy('created_at', 'DESC')->get();
+        $data = [
+            'posts' => $posts
+        ];
+        return view('admin.posts.index', $data);
     }
 }
